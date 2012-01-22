bc5b20b5883c6fde5987de0958a416838698e647
Patrick Roberts <pat2060@sbcglobal.net>
2012-01-22 15:55:36 -0600

    Added fully functional Javascript parser

--- Complex.js
+++ Complex.js
@@ -32,6 +32,7 @@ function Complex(r,i,m,t)
 }	}
 
 // Constants
+Complex["0"] = new Complex(0,0,0,0);
 Complex["1"] = new Complex(1,0,1,0);
 Complex["I"] = new Complex(0,1,1,Math.PI/2);
 Complex["PI"]= new Complex(Math.PI,0,Math.PI,0);
@@ -298,4 +299,298 @@ Complex.fPart = function(c)
 	var i = c.i-Math.floor(c.i);
 	i = i<0?i+1:i;
 	return new Complex(re,i);
+}
+
+// Locates the index of the next addition or subtraction
+// operator that is not nested within another function
+String.prototype.nextAddSub = function(i)
+{	var nests=0;
+	for(c=i;c<this.length;c++)
+	{	if(this[c]=="(")
+			nests++;
+		if(this[c]==")")
+			nests--;
+		if(nests==0 && (this[c]=="+" || (this[c]=="-" && "+-*/^ ".indexOf(this[Math.max(c-1,0)])==-1))|| nests<0)
+			return c;
+	}
+	return this.length;
+}
+
+// Locates the index of the next multiplication or division
+// operator that is not nested within another function
+String.prototype.nextMultDiv = function(i)
+{	var nests=0;
+	for(c=i;c<this.length;c++)
+	{	if(this[c]=="(")
+			nests++;
+		if(this[c]==")")
+			nests--;
+		if(nests==0 &&(this[c]=="*" || this[c]=="/" || this[c]==" ")|| nests<0)
+			return c;
+	}
+	return this.length;
+}
+
+// Locates the index of the next power operator
+// that is not nested within another function
+String.prototype.nextPower = function(i)
+{	var nests=0;
+	for(c=i;c<this.length;c++)
+	{	if(this[c]=="(")
+			nests++;
+		if(this[c]==")")
+			nests--;
+		if(nests==0 && this[c]=="^" || nests<0)
+			return c;
+	}
+	return this.length;
+}
+
+// Locates the next index of a close parenthesis
+// in order to find the end of a function
+String.prototype.lastCloseParen = function(i)
+{	for(c=this.length-1;c>=i;c--)
+	{	if(this[c]==")")
+			return c;
+	}
+	return this.length;
+}
+
+// Locates the last index of a digit in a string
+// of consecutive digits, then stores the digits
+// as a Complex
+String.prototype.lastDigit = function(i)
+{	var f=parseFloat(this);
+	return (isNaN(f)?"":new String(f)).length+i;
+}
+
+// Parses a mathematical expression (not an equation)
+// and returns the Javascript as a String
+// str:String
+//		The input (e.g. the mathematical expression)
+// closeParen:Boolean
+//		wrap the code with a predefined command to
+//		make it storable in a function
+// last:Number
+//		when calling this function, set this value to 0
+//		in order for it to parse correctly
+// config:Array
+//		an Array containing the names of variables that
+//		will be passed to the resulting function as
+//		arguments
+Complex.parseFunction = function parseFunction(str,closeParen,last,config)
+{	var vars = ["e","i","pi"];
+	vars = (typeof config == "object" && config instanceof Array)?vars.concat(config):vars;
+	var functions = ["cos(","sin(","tan(","sec(","csc(","cot(","log(","ln(","(",
+					"e^(","arccos(","arcsin(","arctan(","arcsec(","arccsc(","arccot(",
+					"-","sinh(","cosh(","tanh(","csch(","sech(","coth(",
+					"re(","im(","abs(","arg(","int(","floor(","ceil(","round(",
+					"fpart(","ipart(","sqrt(","arcsinh(","arccosh(","arctanh(",
+					"arcsech(","arccsch(","arccoth("];
+	var operators = ["+","-","*","/","^"," "];
+	var digits = ["0","1","2","3","4","5","6","7","8","9","."];
+	var close= ")";
+	var fStr = closeParen?"var i=Complex.I;var e=Complex.E;var pi=Complex.PI;return ":"";
+	var i=0;
+	var lastObj = last;
+	while(i<str.length)
+	{	var skip = false;
+		for(var c=0;c<functions.length;c++)
+		{	if(str.substr(i,functions[c].length)==functions[c] && lastObj!=2)
+			{	i+=functions[c].length;
+				var temp = parseFunction(str.substring(i,c==16?str.nextAddSub(i):str.lastCloseParen()),false,c==16?1:0,config);
+				switch(c)
+				{	case 0:
+						fStr+="Complex.cos(";
+						break;
+					case 1:
+						fStr+="Complex.sin(";
+						break;
+					case 2:
+						fStr+="Complex.tan(";
+						break;
+					case 3:
+						fStr+="Complex.sec(";
+						break;
+					case 4:
+						fStr+="Complex.csc(";
+						break;
+					case 5:
+						fStr+="Complex.cot(";
+						break;
+					case 6:
+						fStr+="Complex.log(";
+						break;
+					case 7:
+						fStr+="Complex.log(";
+						break;
+					case 8:
+						fStr+="(";
+						break;
+					case 9:
+						fStr+="Complex.exp(";
+						break;
+					case 10:
+						fStr+="Complex.arccos(";
+						break;
+					case 11:
+						fStr+="Complex.arcsin(";
+						break;
+					case 12:
+						fStr+="Complex.arctan(";
+						break;
+					case 13:
+						fStr+="Complex.arcsec(";
+						break;
+					case 14:
+						fStr+="Complex.arccsc(";
+						break;
+					case 15:
+						fStr+="Complex.arccot(";
+						break;
+					case 16:
+						fStr+="Complex.neg(";
+						break;
+					case 17:
+						fStr+="Complex.sinh(";
+						break;
+					case 18:
+						fStr+="Complex.cosh(";
+						break;
+					case 19:
+						fStr+="Complex.tanh(";
+						break;
+					case 20:
+						fStr+="Complex.csch(";
+						break;
+					case 21:
+						fStr+="Complex.sech(";
+						break;
+					case 22:
+						fStr+="Complex.coth(";
+						break;
+					case 23:
+						fStr+="Complex.re(";
+						break;
+					case 24:
+						fStr+="Complex.im(";
+						break;
+					case 25:
+						fStr+="Complex.abs(";
+						break;
+					case 26:
+						fStr+="Complex.arg(";
+						break;
+					case 27:
+						fStr+="Complex.floor(";
+						break;
+					case 28:
+						fStr+="Complex.floor(";
+						break;
+					case 29:
+						fStr+="Complex.ceil(";
+						break;
+					case 30:
+						fStr+="Complex.round(";
+						break;
+					case 31:
+						fStr+="Complex.fPart(";
+						break;
+					case 32:
+						fStr+="Complex.floor(";
+						break;
+					case 33:
+						fStr+="Complex.sqrt(";
+						break;
+					case 34:
+						fStr+="Complex.arcsinh(";
+						break;
+					case 35:
+						fStr+="Complex.arccosh(";
+						break;
+					case 36:
+						fStr+="Complex.arctanh(";
+						break;
+					case 37:
+						fStr+="Complex.arcsech(";
+						break;
+					case 38:
+						fStr+="Complex.arccsch(";
+						break;
+					case 39:
+						fStr+="Complex.arccoth(";
+						break;
+				}
+				fStr+=temp[0]+")";
+				i+=temp[1];
+				skip = true;
+				lastObj = temp[2];
+				break;
+		}	}
+		if(skip)
+			continue;
+		for(var c=0;c<operators.length;c++)
+		{	if(str.substr(i,1)==operators[c] && lastObj==2)
+			{	i++;
+				var temp;
+				switch(c)
+				{	case 0:
+						temp = parseFunction(str.substring(i,str.nextAddSub(i)),false,1,config);
+						fStr+=".add(";
+						break;
+					case 1:
+						temp = parseFunction(str.substring(i,str.nextAddSub(i)),false,1,config);
+						fStr+=".sub(";
+						break;
+					case 2:
+						temp = parseFunction(str.substring(i,Math.min(str.nextMultDiv(i),str.nextAddSub(i))),false,1,config);
+						fStr+=".mult(";
+						break;
+					case 3:
+						temp = parseFunction(str.substring(i,Math.min(str.nextMultDiv(i),str.nextAddSub(i))),false,1,config);
+						fStr+=".divBy(";
+						break;
+					case 4:
+						temp = parseFunction(str.substring(i,Math.min(str.nextMultDiv(i),str.nextAddSub(i),str.nextPower(i))),false,1,config);
+						fStr+=".cPow(";
+						break;
+					case 5:
+						temp = parseFunction(str.substring(i,Math.min(str.nextMultDiv(i),str.nextAddSub(i))),false,1,config);
+						fStr+=".mult(";
+						break;
+				}
+				fStr+=temp[0]+")"
+				skip = true;
+				i+=temp[1];
+				lastObj = temp[2];
+				break;
+		}	}
+		if(skip)
+			continue;
+		for(var c=0;c<vars.length;c++)
+		{	if(str.substr(i,vars[c].length)==vars[c] && lastObj!=2)
+			{	fStr+=vars[c];
+				i+=vars[c].length;
+				skip = true;
+				lastObj = 2;
+				break;
+		}	}
+		if(skip)
+			continue;
+		for(var c=0;c<digits.length;c++)
+		{	if(str.substr(i,1)==digits[c])
+			{	i=str.lastDigit(i);
+				var f = parseFloat(str);
+				fStr+="new Complex("+f+",0,"+f+",0)";
+				skip = true;
+				lastObj = 2;
+				break;
+		}	}
+		if(str.substr(i,1)==close)
+			return [fStr+(closeParen?";":""),i+(last==0?1:0),lastObj];
+		if(!skip)
+		{	i++;
+			continue;
+	}	}
+	return closeParen?fStr+";":[fStr,str.length,lastObj];
 }

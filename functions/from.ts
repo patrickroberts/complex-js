9b5afc61f127b1e44e289cda9259fe158271aa15
Patrick Roberts <pat2060@sbcglobal.net>
2020-02-23 23:49:56 -0600

    Optimized from to avoid precomputing uninitialized members

--- functions/from.ts
+++ functions/from.ts
@@ -1,20 +1,14 @@
 import { Complex, ComplexConstructor } from '../internal/complex';
 import Mask from '../internal/mask';
-import absImpl from '../internal/abs';
-import argImpl from '../internal/arg';
-import getReal from '../methods/real';
-import getImag from '../methods/imag';
-import getAbs from '../methods/abs';
-import getArg from '../methods/arg';
 
 export default function from<T extends Complex> (Complex: ComplexConstructor<T>, z: Complex | number, imag: number = 0): T {
-  let zReal: number, zImag: number, zAbs: number, zArg: number;
+  let zReal: number, zImag: number, zAbs: number, zArg: number, zMask: Mask;
 
   if (typeof z === 'number') {
-    zReal = z; zImag = imag; zAbs = absImpl(z, imag); zArg = argImpl(z, imag);
+    zReal = z; zImag = imag; zAbs = NaN; zArg = NaN; zMask = Mask.HAS_CARTESIAN;
   } else {
-    zReal = getReal(z); zImag = getImag(z); zAbs = getAbs(z); zArg = getArg(z);
+    zReal = z._real; zImag = z._imag; zAbs = z._abs; zArg = z._arg; zMask = z._mask;
   }
 
-  return new Complex(zReal, zImag, zAbs, zArg, Mask.HAS_ALL);
+  return new Complex(zReal, zImag, zAbs, zArg, zMask);
 }
